8ba6ad91a1725d3e0cac5cee1359dffa891981e3
alexandra <le.onze.mai@gmail.com>
2019-06-28 21:25:12 +0300

    made adaptive footer

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 const $buttonOpenMenu = document.querySelector('.js-open-menu');
+const $copyrightYear = document.querySelector('.js-copyright-year');
+
+$copyrightYear.textContent = '2019';
 
 $buttonOpenMenu.addEventListener('click', function () {
   const $menu = document.querySelector('.js-menu');

17a3c9bafb813af84eceb8618de705147537b6de
alexandra <le.onze.mai@gmail.com>
2019-06-30 18:16:50 +0300

    made slider

--- js/script.js
+++ js/script.js
@@ -7,3 +7,33 @@ $buttonOpenMenu.addEventListener('click', function () {
   const $menu = document.querySelector('.js-menu');
   $menu.classList.toggle('menu--show');
 });
+
+$(document).ready( function() {
+  $('.js-clients-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1032,
+        settings: {
+          slidesToShow: 3,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 2,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          dots: true
+        }
+      }
+    ]
+  });
+});
